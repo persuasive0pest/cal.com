b9bc2c5d8caaba8a45e7c4d709c2a68f41bb83cf
persuasive0pest <160201971+persuasive0pest@users.noreply.github.com>
2024-05-05 17:23:51 -0400

    Update new.tsx to use getServerSideProps

--- apps/web/pages/auth/new.tsx
+++ apps/web/pages/auth/new.tsx
@@ -1,6 +1,13 @@
-export default function NewUserPage() {
+export const getServerSideProps = async () => {
+  const props = {
+    nextPublicWebappUrl: process.env.NEXT_PUBLIC_WEBAPP_URL,
+  };
+  return { props };
+};
+
+export default function NewUserPage({ nextPublicWebappUrl }: { nextPublicWebappUrl: string }) {
   if (typeof window !== "undefined") {
-    window.location.assign(process.env.NEXT_PUBLIC_WEBAPP_URL || "https://app.cal.com");
+    window.location.assign(nextPublicWebappUrl || "https://app.cal.com");
   }
   return null;
 }
